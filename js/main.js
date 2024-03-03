8a8832cca954b063f20adcf2f35fa99c9e872b4f
AbdullaAllNoman <abdullaalnoman14@gmail.com>
2024-03-03 02:16:01 -0500

    repo

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// window.onscroll = function() {myFunction()};
+
+// var header = document.getElementById("myHeader");
+// var sticky = header.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
+
+// class MyFooter extends HTMLElement{
+//  connectedCallback(){
+//     this.innerHTML()=`
+//     <footer>
+//       <p>&copy; Copyright 2022</p>
+//       <a href="#">Abdulla All Noman</a>
+//     </footer>
+//     `
+//  }
+// }
+// customElements.define('my-footer',MyFooter)
